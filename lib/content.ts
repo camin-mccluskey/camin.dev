765e27701aaa1d176096a4727f4e07b5f4823c30
camin-mccluskey <mccluskey.camin@gmail.com>
2025-03-23 22:04:13 +0800

    Filter all posts by visibility

--- lib/content.ts
+++ lib/content.ts
@@ -1,3 +1,3 @@
 import { allPosts as allPlain, allPostMdxes } from 'contentlayer/generated'
 
-export const allPosts = [...allPlain, ...allPostMdxes]
+export const allPosts = [...allPlain, ...allPostMdxes].filter((p) => !p.isDraft)
